1b9f7d15ea313684993f62e633aa06202aa0ca02
Matthew Aguirre <matt.aguirre@gmail.com>
2017-12-08 12:02:40 -0500

    Added TRANSMITTER

--- cnrsniffer/src/main/java/com/artistech/cnr/Sniffer.java
+++ cnrsniffer/src/main/java/com/artistech/cnr/Sniffer.java
@@ -1,5 +1,6 @@
 package com.artistech.cnr;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
@@ -7,9 +8,8 @@ import java.nio.ByteBuffer;
 import java.util.List;
 
 import edu.nps.moves.dis.OneByteChunk;
-import edu.nps.moves.dis.Pdu;
 import edu.nps.moves.disenum.PduType;
-import edu.nps.moves.disutil.PduFactory;
+import edu.nps.moves.dismobile.TransmitterPdu;
 import edu.nps.moves.dis.SignalPdu;
 
 public class Sniffer {
@@ -18,48 +18,59 @@ public class Sniffer {
 
     public static void main(String[] args) throws Exception {
         InetAddress group = InetAddress.getByName(MCAST_GRP);
-        MulticastSocket ms = new MulticastSocket(MCAST_PORT);
+        final MulticastSocket ms = new MulticastSocket(MCAST_PORT);
 //        ms.setInterface(InetAddress.getByName(InetAddress.getLocalHost().getHostName()));
         ms.joinGroup(group);
 
         byte[] buffer = new byte[8192];
         System.out.println("receiving...");
-        RawAudioPlay rap = new RawAudioPlay();
+        final RawAudioPlay rap = new RawAudioPlay();
         boolean cont = true;
-        Pdu pdu;
-        Pdu copyPdu;
-        PduFactory pduf = new PduFactory();
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("closing...");
+            rap.close();
+            try {
+                ms.leaveGroup(group);
+                ms.close();
+            } catch(IOException ex) {
+            }
+        }));
 
         //audio is: 16-bit Linear PCM 2's complement, Big Endian (4) <- ENCODING SCHEME 4
 
         while (cont) {
             DatagramPacket dp = new DatagramPacket(buffer, buffer.length);
             ms.receive(dp);
-            System.out.println(dp.getAddress().getHostName()+ ":" + dp.getPort());
             byte[] data = dp.getData();
             int pduType = 255 & data[2];
             PduType pduTypeEnum = PduType.lookup[pduType];
             ByteBuffer buf = ByteBuffer.wrap(data);
+
+            System.out.println(pduTypeEnum);
+            System.out.println(dp.getAddress().getHostName()+ ":" + dp.getPort());
             switch(pduTypeEnum) {
                 case SIGNAL:
                     SignalPdu spdu = new SignalPdu();
                     spdu.unmarshal(buf);
+
                     //this is identical to data pulled form WireShark, so that's good.
                     //signal data
                     System.out.println("Data Length: " + spdu.getDataLength());         // Data Length: 2560
-                    System.out.println("Radio ID: " + spdu.getRadioId());               // ID: 11761
                     System.out.println("Sample Rage: " + spdu.getSampleRate());         // 44100 [Hz]
                     System.out.println("Encoding Scheme: " + spdu.getEncodingScheme()); // 4
-                    System.out.println("Entity ID: " + spdu.getEntityId().getEntity()); // 0
                     System.out.println("Num Samples: " + spdu.getSamples());            // 160
                     List<OneByteChunk> d = spdu.getData();
 
+                    System.out.println("Radio ID: " + spdu.getRadioId());               // ID: 11761
+                    System.out.println("Entity ID: " + spdu.getEntityId().getEntity()); // 0
+
                     //header data
-                    System.out.println("Padding: " + spdu.getPadding());                // 0
+//                    System.out.println("Padding: " + spdu.getPadding());                // 0
                     System.out.println("Proto Family: " + spdu.getProtocolFamily());    // 4
-                    System.out.println("Exercise ID: " + spdu.getExerciseID());        //0
                     System.out.println("Proto Version: " + spdu.getProtocolVersion());  // 6
-                    System.out.println("PDU Length: " + spdu.getPduLength());           //352
+//                    System.out.println("PDU Length: " + spdu.getPduLength());           //352
+                    System.out.println("Exercise ID: " + spdu.getExerciseID());         //0
                     System.out.println("Time Stamp: " + spdu.getTimestamp());           //
 
 //                    byte[] b = new byte[d.size()];
@@ -70,20 +81,25 @@ public class Sniffer {
 //                    rap.play(b);
                     break;
                 case TRANSMITTER:
+                    TransmitterPdu tpdu = new TransmitterPdu();
+                    tpdu.unmarshal(buf);
+                    System.out.println("Input Source: " + tpdu.getInputSource());
+                    System.out.println("Frequency: " + tpdu.getFrequency());
+                    System.out.println("Transmit Frequency Bandwidth: " + tpdu.getTransmitFrequencyBandwidth());
+
+                    System.out.println("Radio ID:" + tpdu.getRadioId());
+                    System.out.println("Entity ID:" + tpdu.getEntityId().getEntity());
+
+                    System.out.println("Protocol Family:" + tpdu.getProtocolFamily());
+                    System.out.println("Protocol Version:" + tpdu.getProtocolVersion());
+                    System.out.println("Exercise ID:" + tpdu.getExerciseID());
+                    System.out.println("Time Stamp:" + tpdu.getTimestamp());
+
+                    break;
+                default:
+                    System.out.println("Unknown Type:" + pduTypeEnum);
                     break;
             }
-
-            System.out.println(pduTypeEnum);
-//            rap.play(b);
-//            pdu = pduf.createPdu(data);
-//            System.out.println(pdu);
-
-//            String s = new String(dp.getData());
-//            System.out.println(s);
         }
-        rap.close();
-        // ms.leaveGroup(group);
-        // ms.close();
-
     }
 }
